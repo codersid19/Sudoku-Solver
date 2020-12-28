b3c2bf4036905cb8b1002d64d1702b6a4e35bd4c
codersid19 <shirodkar.siddhant19@gmail.com>
2020-12-28 10:27:59 +0530

    Check Board

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -11,6 +11,26 @@ board = [
 ]
 
 
+def right_Number(bo, num, position):
+    for i in range(bo[0]):
+        if bo[position[0]][i] == num and position[1] != i:
+            return False
+
+    for i in range(bo[0]):
+        if bo[i][position[1]] == num and position[0] != i:
+            return False
+
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != position:
+                return False
+
+    return True
+
+
 def printBo(bo):
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
